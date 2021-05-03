cc0d8b5f5b562af8c53f726e4185c46426bf1e18
kazumasamatsumoto <k.matsumoto@benjamin.co.jp>
2021-05-03 12:02:18 +0900

    sample login component attached

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 
 export default function Layout({ children, title = "Default title" }) {
   return (
-    <div className="flex justify-center items-center flex-col min-h-screen text-white font-mono bg-gray-800">
+    <div className="flex justify-center items-center flex-col min-h-screen text-white font-mono bg-gray-100">
       <Head>
         <title>{title}</title>
       </Head>
