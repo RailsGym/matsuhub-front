9537e320be5b8fccbf4f8063070fab7b8d70944c
kazumasamatsumoto <k.matsumoto@benjamin.co.jp>
2021-05-03 16:08:37 +0900

    header update

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header({ title = "Default title" }) {
       </Head>
       <main className="flex flex-1 w-screen flex-col">
         <div className="bg-gray-100">
-          <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+          <div className="py-3 px-3">
             <div className="flex justify-between flex-wrap">
               <div className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-indigo-50">
                 テスト
@@ -27,7 +27,7 @@ export default function Header({ title = "Default title" }) {
                   />
                 </svg>
               </div>
-              <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+              <div className="">
                 <a
                   href="#"
                   className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-indigo-50"
