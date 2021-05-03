a74a8ffd22b165121000e76842c99ba6ae0e41d7
kazumasamatsumoto <k.matsumoto@benjamin.co.jp>
2021-05-03 14:34:15 +0900

    auth code review

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -6,7 +6,7 @@ const cookie = new Cookie();
 
 export default function Auth() {
   const router = useRouter();
-  const [mail, setMail] = useState("");
+  const [email, setMail] = useState("");
   const [password, setPassword] = useState("");
   const [isLogin, setIsLogin] = useState(true);
 
@@ -16,7 +16,12 @@ export default function Auth() {
         `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/vi/auth/sign_in/`,
         {
           method: "POST",
-          body: JSON.stringify({ mail: mail, password: password }),
+          body: JSON.stringify({
+            email: email,
+            name: "Trao",
+            password: password,
+            password_confirmation: password,
+          }),
           headers: {
             "Content-Type": "application/json",
           },
@@ -42,19 +47,16 @@ export default function Auth() {
   const authUser = async (e) => {
     e.preventDefault();
     if (isLogin) {
-      login;
+      login();
     } else {
       try {
-        await fetch(
-          `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/v1/auth/sign_up/`,
-          {
-            method: "POST",
-            body: JSON.stringify({ mail: mail, password: password }),
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        ).then((res) => {
+        await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/v1/auth/`, {
+          method: "POST",
+          body: JSON.stringify({ email: email, password: password }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }).then((res) => {
           if (res.status === 400) {
             throw "authentication failed";
           }
@@ -89,7 +91,7 @@ export default function Auth() {
             required
             className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
             placeholder="email address"
-            value={mail}
+            value={email}
             onChange={(e) => {
               setMail(e.target.value);
             }}
@@ -128,21 +130,6 @@ export default function Auth() {
             type="submit"
             className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
-            <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-              <svg
-                className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </span>
             {isLogin ? "ログイン" : "新規ユーザー作成"}
           </button>
         </div>
