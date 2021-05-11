408f93df79cb4f90676620737e01451fbc6624c0
michiwo1 <mk1104kado@gmail.com>
2021-05-11 09:05:44 +0900

    styled-componentを設定, canvas/new ページを作成

---
+++ pages/canvas/new.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const SContainer = styled.div`
+  margin: auto;
+  text-align: center;
+  margin-top: 80px;
+  width: 500px;
+  height: 321px;
+  border: 1px solid #d8d8d8;
+  border-radius: 5px;
+`;
+const STitle = styled.h1`
+  font-size: 24px;
+  margin: 30px 0;
+`;
+const SNameLabel = styled.p`
+  text-align: left;
+  margin-left: 40px;
+  font-size: 12px;
+  color: #b7b6b6;
+`;
+const SCanvasTitleInput = styled.input`
+  width: 418px;
+  height: 48px;
+  margin-bottom: 45px;
+  background-color: #f7f9f9;
+  border: 1px solid #d8d8d8;
+  border-radius: 5px;
+`;
+const SCreateButton = styled.button`
+  width: 418px;
+  height: 48px;
+  background-color: #13b1c0;
+  border-radius: 5px;
+  color: #ffffff;
+  ${({ disabled }) =>
+    disabled &&
+    `
+    opacity: 0.5;
+    cursor: default;
+  `}
+`;
+
+export default function CreateCanvasCard() {
+  const [canvasTitle, setCanvasTitle] = useState();
+  const handleInputChange = event => {
+    setCanvasTitle(event.target.value);
+  };
+  const createCanvas = () => {
+    alert(canvasTitle);
+  };
+  return (
+    <SContainer>
+      <STitle>キャンバス新規作成</STitle>
+      <SNameLabel>名前</SNameLabel>
+      <SCanvasTitleInput type="text" onChange={handleInputChange} />
+      <SCreateButton disabled={!canvasTitle} onClick={createCanvas}>
+        新規作成
+      </SCreateButton>
+    </SContainer>
+  );
+}
