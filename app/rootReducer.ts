8181682172cf50e47bf4a8d68c3ede8fc5191f88
michiwo1 <mk1104kado@gmail.com>
2021-06-04 16:44:42 +0900

    labelSlice.ts を作成

--- app/rootReducer.ts
+++ app/rootReducer.ts
@@ -4,12 +4,14 @@ import loginUserReducer from 'features/loginUser/LoginUserSlice';
 import signUpUserReducer from 'features/signUpUser/SignUpUserSlice';
 import canvasesReducer from 'features/canvases/canvasesSlice';
 import canvasReducer from 'features/canvases/canvasSlice';
+import labelReducer from 'features/labels/labelSlice';
 
 const rootReducer = combineReducers({
   loginUser: loginUserReducer,
   canvases: canvasesReducer,
   signedUpUser: signUpUserReducer,
   canvas: canvasReducer,
+  label: labelReducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
