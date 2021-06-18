ff17dfc7c59203d9457dc06f4c64cd67978def72
michiwo1 <mk1104kado@gmail.com>
2021-06-18 10:43:53 +0900

    名前のフォームを微修正

--- pages/sign_up.tsx
+++ pages/sign_up.tsx
@@ -77,7 +77,7 @@ export default function SignUp() {
               type="username"
               autoComplete="current-password"
               required
-              className="appearance-none rounded-none relative block w-full px-3 py-2 bg-gray-50 border border-gray-300 text-gray-900 rounded-md focus:outline-none focus:ring-customgreen focus:border-customgreen focus:z-10 sm:text-sm"
+              className="appearance-none rounded-none relative block w-full px-3 py-2 bg-gray-50 border border-gray-300 text-gray-900 rounded-md focus:outline-none focus:ring-customgreen focus:border-customgreen focus:z-10 focus:ring-1 sm:text-sm "
               value={username}
               onChange={(e) => {
                 setUsername(e.target.value);
